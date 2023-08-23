42b7bfcc81f43edd4002547f2eb7538e8da16931
jaco266w <jaco266w@stud.kea.dk>
2023-08-23 09:43:59 +0200

    done

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const buttons = document.querySelector("#buttons");
 const p1 = document.querySelector("#player1");
 const p2 = document.querySelector("#player2"); 
 
+let element;
 let mTal;
 
 window.addEventListener("load", sidenVises);
@@ -21,60 +22,48 @@ function sidenVises() {
     p1.classList = "player";
     p2.classList = "player";
 
-
-    rock.addEventListener("click", sten);
-    paper.addEventListener("click", papir);
-    scissors.addEventListener("click", saks);
+    rock.addEventListener("click", start);
+    paper.addEventListener("click", start);
+    scissors.addEventListener("click", start);
 }
 
-function sten() {
-    console.log("sten");
-    rock.removeEventListener("click", sten);
-    buttons.classList.add("disabled");
-    p1.classList.add("shake");
-    p2.classList.add("shake");
-    p1.addEventListener("animationend", sten2);
-}
-
-function sten2() {
-    console.log("sten2")
-    p1.classList.remove("shake");
-    p1.classList.add("rock");
+function start() {
+    console.log("start");
+    console.log(this);
     
-    modstander();
-    end();
-}
+    buttons.classList = " ";
+    p1.classList = "player";
+    p2.classList = "player";
 
-function papir() {
-    console.log("pair");
-    rock.removeEventListener("click", papir);
-    buttons.classList.add("disabled");
-    p1.classList.add("shake");
-    p2.classList.add("shake");
-    p1.addEventListener("animationend", papir2);
-}
+    win.classList.add("hidden");
+    draw.classList.add("hidden");
+    lose.classList.add("hidden");
 
-function papir2() {
-    console.log("papir2")
-    p1.classList.remove("shake");
-    p1.classList.add("paper");
+    rock.removeEventListener("click", start);
+    paper.removeEventListener("click", start);
+    scissors.removeEventListener("click", start);
     
-    modstander();
-    end();
-}
-function saks() {
-    console.log("pair");
-    rock.removeEventListener("click", saks);
+    element = this;
+
     buttons.classList.add("disabled");
     p1.classList.add("shake");
     p2.classList.add("shake");
-    p1.addEventListener("animationend", saks2);
+    p1.addEventListener("animationend", show);
 }
 
-function saks2() {
-    console.log("saks2")
-    p1.classList.remove("shake");
-    p1.classList.add("scissors");
+function show() {
+    console.log("show")
+    console.log(element);
+    p1.classList = "player";
+    if (element.classList.contains("rock")) {
+        p1.classList.add("rock");
+    }
+    else if (element.classList.contains("paper")) {
+        p1.classList.add("paper");
+    }
+    else {
+        p1.classList.add("scissors");
+    }
     
     modstander();
     end();
@@ -82,7 +71,7 @@ function saks2() {
 
 function modstander() {
     console.log("modstander");
-    p2.classList.remove("shake");
+    p2.classList = "player";
      mTal = Math.floor(Math.random() * 3) + 1;
      console.log(mTal);
     if (mTal == 1) {
@@ -98,29 +87,52 @@ function end() {
     console.log("end");
     if (p1.classList.contains("rock") && p2.classList.contains("scissors")) {
         win.classList.remove("hidden");
+        draw.classList.add("hidden");
+        lose.classList.add("hidden");
     }
     else if (p1.classList.contains("rock") && p2.classList.contains("rock")) {
         draw.classList.remove("hidden");
+        win.classList.add("hidden");
+        lose.classList.add("hidden");
     }
     
     else if (p1.classList.contains("paper") && p2.classList.contains("rock")) {
         win.classList.remove("hidden");
+        draw.classList.add("hidden");
+        lose.classList.add("hidden");
     }
     
     else if (p1.classList.contains("paper") && p2.classList.contains("paper")) {
         draw.classList.remove("hidden");
+        win.classList.add("hidden");
+        lose.classList.add("hidden");
     }
 
     else if (p1.classList.contains("scissors") && p2.classList.contains("paper")) {
         win.classList.remove("hidden");
+        draw.classList.add("hidden");
+        lose.classList.add("hidden");
     }
     
     else if (p1.classList.contains("scissors") && p2.classList.contains("scissors")) {
         draw.classList.remove("hidden");
+        win.classList.add("hidden");
+        lose.classList.add("hidden");
     }
 
     else {
         lose.classList.remove("hidden");
+        draw.classList.add("hidden");
+        win.classList.add("hidden");
     }
+    restart();
+}
+
+function restart() {
+    console.log("restart");
+    buttons.classList.remove("disabled");
+    rock.addEventListener("click", start);
+    paper.addEventListener("click", start);
+    scissors.addEventListener("click", start);
 }
 
